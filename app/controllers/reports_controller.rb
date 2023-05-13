705240059a197bd14f5e6e086f8b4f98532ed777
nicole2525 <nicole2525nomail@gmail.com>
2023-05-13 20:10:42 +0900

    日報編集時に言及レコードも更新されるようにした

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -40,7 +40,32 @@ class ReportsController < ApplicationController
   end
 
   def update
+    # 1.更新前に言及している日報を確認
+    mentioning_before_update = @report.mentioning_reports.map {|r| r.id }
+
     if @report.update(report_params)
+      # 2.更新する日報の言及先を確認
+      mentioning_after_update = @report.content.scan(/http:\/\/localhost:3000\/reports\/(\d+)/).uniq.flatten.map{ |r| r.to_i }
+      
+      #消えた言及先と追加された言及を調べる
+      missing = missing_mentions(mentioning_before_update, mentioning_after_update)
+      added = added_mentions(mentioning_after_update, mentioning_before_update)
+      
+      #消えた言及先があれば削除
+      if missing.any?
+        missing.each do |m|
+          destroy_target = Mention.find_by(mentioning_report_id: @report.id, mentioned_report_id: m.to_i)
+          destroy_target.destroy
+        end
+      end
+
+      #追加された言及先があれば追加
+      if added.any?
+        added.each do |a|
+          @mention = Mention.new(mentioning_report_id: @report.id, mentioned_report_id: a.to_i)
+          @mention.save
+        end
+      end
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
@@ -66,5 +91,13 @@ class ReportsController < ApplicationController
   def contains_mentions?
     host = "http://localhost:3000/" # 定数にするか要検討
     @report.content.include?(host)
+  end
+
+  def missing_mentions(before, after)
+    before - after
+  end
+
+  def added_mentions(after, before)
+    after - before
   end
 end
