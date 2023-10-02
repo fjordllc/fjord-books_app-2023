e76a8a1dbb386b54b59388be09e001091971b07a
Kuroneko <ken.aster8@gmail.com>
2023-10-02 16:02:44 +0900

    reportのエラー表示が正しく機能していなかったため修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,11 +3,13 @@
 class ReportsController < ApplicationController
   before_action :set_report, only: %i[show edit update destroy]
   before_action :check_user, only: %i[edit update destroy]
+
   def index
     @reports = Report.order(:id).page(params[:page])
   end
 
   def show
+    @comments = @report.comments
   end
 
   def new
@@ -20,30 +22,26 @@ class ReportsController < ApplicationController
   def create
     @report = Report.new(report_params)
 
-    respond_to do |format|
-      if @report.save
-        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human) }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @report.save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human) }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
-    end
+    if @report.update(report_params)
+      redirect_to report_url(@report), notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
+  end
   end
 
   def destroy
     @report.destroy
 
     respond_to do |format|
-      format.html { redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human) }
+      redirect_to reports_url, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
     end
   end
 
