5a9ebf49afd1cecba335311046ecb55674c896f8
ayu-0505 <amehuri.boshi.sh@icloud.com>
2023-11-15 21:52:07 +0900

    リサイズ方法を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_one_attached :avatar
+  has_one_attached :avatar do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :avatar, attached: true, content_type: { in: ['image/png', 'image/jpeg', 'image/gif'], message: 'はpng, jpg, jpeg, gif のいずれかの形式にして下さい。' }
+  validates :avatar, content_type: { in: ['image/png', 'image/jpeg', 'image/gif'], message: 'はpng, jpg, jpeg, gif のいずれかの形式にして下さい。' }
 end
